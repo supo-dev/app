0d9466b8cb1b64dc951efe0c2eff09a7cad8b61e
Javier Reyes <javier.reys@gmail.com>
2025-09-30 12:00:45 +0200

    feat: add username generation to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ final class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'username' => fake()->unique()->userName(),
             'email_verified_at' => now(),
             'password' => self::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
