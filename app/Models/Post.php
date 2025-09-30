b765677f7ac8c8e73903d290997ab0a5aed8777c
Punyapal Shah <mrpunyapal@gmail.com>
2025-09-30 20:47:13 +0530

    fix(model): update property annotations in Post model for clarity

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,15 +5,21 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
 /**
- * @property-read int $id
- * @property-read int $user_id
- * @property-read string $content
+ * @property int $id
+ * @property int $user_id
+ * @property string $content
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read User $user
+ * @property-read Collection<int, Like> $likes
  */
 final class Post extends Model
 {
