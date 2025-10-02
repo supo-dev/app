9560ee486104b93f8f4c354793d590aac7e6d092
Punyapal Shah <mrpunyapal@gmail.com>
2025-10-02 17:57:05 +0530

    feat: add ForYouFeed query class and corresponding unit tests

---
+++ app/Queries/ForYouFeed.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Queries;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Builder;
+
+final readonly class ForYouFeed
+{
+    /**
+     * @return Builder<Post>
+     */
+    public function builder(): Builder
+    {
+        return Post::query()
+            ->with(['user', 'likes'])
+            ->latest('updated_at');
+    }
+}
